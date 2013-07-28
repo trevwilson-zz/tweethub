7ef7e61b523deda9ba23730736423dab1090a450
Trevor <trevwilson16@gmail.com>
2013-07-28 17:07:16 -0400

    Add microposts and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -29,6 +30,10 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
